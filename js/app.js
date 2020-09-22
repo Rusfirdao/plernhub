4924454ab04d7a6710107dc3314ee9c33e377283
Rusfirdaoo <fiirdaoo@gmail.com>
2020-09-22 18:25:45 +0700

    add search page

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
-new Vue({
-    el: '#app',
-    template: '#search',
-    data() {
-      return {
-        query: ''
-      };
-    }
-  });
+
+  $(function() {
+
+    document.addEventListener('init', function(event) {
+        var page = event.target;
+
+        console.log(page.id);
+        if (page.id === 'Home') {
+            $('#back').hide();
+            page.querySelector('#spiderman').onclick = function() {
+                document.querySelector('#myNavigator').pushPage('views/detail.html');
+
+            };
+            page.querySelector('#infinitywar').onclick = function() {
+                document.querySelector('#myNavigator').pushPage('views/detail2.html');
+            };
+            page.querySelector('#endgame').onclick = function() {
+                document.querySelector('#myNavigator').pushPage('views/detail3.html');
+            };
+        } else if (page.id === 'spiderman' || page.id === 'infinitywar' || page.id === 'endgame') {
+            $('#back').show();
+            document.querySelector('ons-back-button').onclick = function(event) {
+                document.querySelector('#myNavigator').popPage();
+            };
+
+        }
+
+
+
+    });
+})
