e3d67f3fec53f312cfd7ae9961fd295a05ccc73e
Rusfirdaoo <fiirdaoo@gmail.com>
2020-09-23 00:45:33 +0700

    add all detail page

--- js/app.js
+++ js/app.js
@@ -1,31 +1,17 @@
-
-  $(function() {
-
-    document.addEventListener('init', function(event) {
-        var page = event.target;
-
-        console.log(page.id);
-        if (page.id === 'Home') {
-            $('#back').hide();
-            page.querySelector('#spiderman').onclick = function() {
-                document.querySelector('#myNavigator').pushPage('views/detail.html');
-
-            };
-            page.querySelector('#infinitywar').onclick = function() {
-                document.querySelector('#myNavigator').pushPage('views/detail2.html');
-            };
-            page.querySelector('#endgame').onclick = function() {
-                document.querySelector('#myNavigator').pushPage('views/detail3.html');
-            };
-        } else if (page.id === 'spiderman' || page.id === 'infinitywar' || page.id === 'endgame') {
-            $('#back').show();
-            document.querySelector('ons-back-button').onclick = function(event) {
-                document.querySelector('#myNavigator').popPage();
-            };
-
-        }
-
-
-
-    });
-})
+document.addEventListener('init', function(event) {
+    var page = event.target;
+  
+    if (page.id === 'homemovie') {
+      page.querySelector('#Movie1').onclick = function () {
+        document.querySelector('#myNavigator').pushPage('../views/detail1.html')
+    }
+    page.querySelector('#Movie2').onclick = function () {
+        document.querySelector('#myNavigator').pushPage('../views/detail2.html')
+    }
+    page.querySelector('#Movie3').onclick = function () {
+        document.querySelector('#myNavigator').pushPage('../views/detail3.html')
+    }
+    } else if (page.id === 'page2') {
+      page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+    }
+  });
